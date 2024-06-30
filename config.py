1da2f7b4a1d1302c211f8754154d679216639687
𖢵 || 𝗦ʏɴαxx ||𖢵 <159287148+Sagexdd@users.noreply.github.com>
2024-06-30 16:55:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("21827814"))
+API_HASH = getenv("af097d21fa5049b0ed0d614ed3b885cd")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("7369149161:AAFAoyZ818kS-St-mpcy2TTumzFlLFZWAX8")
 #OWNER_ID = int(getenv("7037832227"))
-OWNER_ID = int(getenv("OWNER_ID", "6730120198"))
-MONGO_DB_URI = getenv("MONGO_DB_URI")
+OWNER_ID = int(getenv("OWNER_ID", "7376728791"))
+MONGO_DB_URI = getenv("mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority")
 MUST_JOIN = getenv("MUST_JOIN", None)
